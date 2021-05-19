f0ca476981d315e7a6b757519100dc7c1ce8739b
Rahman Mohammad Hafijur <mohammad-hafijur.rahman@bostad.uppsala.se>
2021-05-20 00:31:45 +0200

    image prediction with mobilenetv2

--- main.py
+++ main.py
@@ -4,13 +4,15 @@ import numpy as np
 
 filename = 'images/cat.jpg'
 # using tensorflow
-img = tf.keras.preprocessing.image.load_img(filename, target_size = (224, 224))
+img = tf.keras.preprocessing.image.load_img(filename, target_size=(224, 224))
 
 # Loading the deep learning model
 # deep_learning_alg = tf.keras.applications.mobilenet.MobileNet()
 deep_learning_alg = tf.keras.applications.mobilenet_v2.MobileNetV2()
 # adding 4th dimension
-final_image = np.expand_dims(img, axis = 0)
+resized_img = tf.keras.preprocessing.image.img_to_array(img)
+# need fourth dimension for the algorithm to predict
+final_image = np.expand_dims(resized_img, axis=0)
 final_image = tf.keras.applications.mobilenet.preprocess_input(final_image)
 predictions = deep_learning_alg.predict(final_image)
 # decoding the predictions
