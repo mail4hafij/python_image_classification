fbe5b887006f8471ad8af0f15b279ed51132197f
Rahman Mohammad Hafijur <mohammad-hafijur.rahman@bostad.uppsala.se>
2021-05-20 00:53:32 +0200

    image classification with mobilenetv2

--- main.py
+++ main.py
@@ -3,18 +3,20 @@ import numpy as np
 
 
 filename = 'images/cat.jpg'
-# using tensorflow
 img = tf.keras.preprocessing.image.load_img(filename, target_size=(224, 224))
+img_to_array = tf.keras.preprocessing.image.img_to_array(img)
+# need fourth dimension for the algorithm to predict
+final_image = np.expand_dims(img_to_array, axis=0)
+final_image = tf.keras.applications.mobilenet.preprocess_input(final_image)
 
 # Loading the deep learning model
 # deep_learning_alg = tf.keras.applications.mobilenet.MobileNet()
 deep_learning_alg = tf.keras.applications.mobilenet_v2.MobileNetV2()
-# adding 4th dimension
-resized_img = tf.keras.preprocessing.image.img_to_array(img)
-# need fourth dimension for the algorithm to predict
-final_image = np.expand_dims(resized_img, axis=0)
-final_image = tf.keras.applications.mobilenet.preprocess_input(final_image)
 predictions = deep_learning_alg.predict(final_image)
+
 # decoding the predictions
-results = tf.keras.applications.imagenet_utils.decode_predictions(predictions)
-print(results)
+for decoded_prediction in tf.keras.applications.imagenet_utils.decode_predictions(predictions):
+    for name, desc, score in decoded_prediction:
+        print('- {} ({:.2f}%)'.format(desc, 100 * score))
+
+
